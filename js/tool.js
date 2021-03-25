d5bdd99689af603ead3e28436945a9b05432dd41
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-03-25 15:07:32 +0100

    workstation

---
+++ js/tool.js
@@ -0,0 +1,24 @@
+// Snack 1
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while
+
+
+
+// Snack 2
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+
+// Snack 3
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+
+
+// Snack 4
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+
+
+// Snack 5 - Bonus
+// Crea due array di numeri che hanno un numero di elementi diversi.
+// Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
